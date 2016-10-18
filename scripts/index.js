0b94f6d74ed2aea7d5b32493a1951d05f895d6da
GiftyMateKole <giftym94@gmail.com>
2016-10-18 11:14:28 +0000

    geo

--- scripts/index.js
+++ scripts/index.js
@@ -56,11 +56,20 @@
 }
 
 	document.getElementById("geo").onclick = function geo () {
-		function onSuccess(position) {
-			 alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n');
-        
-    }
+		// onSuccess Callback 
+    // This method accepts a Position object, which contains the 
+    // current GPS coordinates 
+    // 
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
  
     // onError Callback receives a PositionError object 
     // 
@@ -69,9 +78,8 @@
               'message: ' + error.message + '\n');
     }
  
-    // Options: throw an error if no update is received every 30 seconds. 
-    // 
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+ 
     
 	}
 
